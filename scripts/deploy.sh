85433646881bb9126d0341159f34b44640f581e8
PEM-- <pemarchandet@gmail.com>
2016-10-09 13:56:02 +0200

    Deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,3 +6,5 @@ git commit -am "Rebuild website"
 git filter-branch -f --prune-empty --subdirectory-filter build
 git push -f origin gh-pages
 git checkout -
+git commit -am "Save local changes"
+git push
